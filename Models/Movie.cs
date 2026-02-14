4d6ccc44c31aa3750844ef572efc6c39ba2bcbbe
JalenHu <jalenhu@byu.edu>
2026-02-14 02:08:16 -0700

    Upload

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,19 +5,24 @@ public class Movie
 {
     [Key]
     public int MovieId { get; set; }
-    [Required]
+    [Required(ErrorMessage = "The field is required")]
     public string Category { get; set; }
-    [Required]
+    [Required (ErrorMessage = "The field is required")]
     public string Title { get; set; }
-    
-    [Required]
+    [Required (ErrorMessage = "The field is required.")]
     [Range (1888, 2100, ErrorMessage = "Enter a valid movie year")]
     public int Year { get; set; }
-    [Required]
+    [Required  (ErrorMessage = "The field is required")]
     public string Director { get; set; }
+    [Required  (ErrorMessage = "The field is required")]
     public string Rating { get; set; }
-    public bool Edit { get; set; }
-    public string Lentto { get; set; }
+    
+    
+    public bool Edited { get; set; }
+    
+    public string? LentTo { get; set; }
+    
     [StringLength(25)]
-    public string Note { get; set; }
+    public string? Note { get; set; }
+   
 }
