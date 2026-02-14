b92e67c1674cbb4c573d3cca5556ff9752fda6d4
JalenHu <jalenhu@byu.edu>
2026-02-14 02:17:32 -0700

    final

--- obj/Debug/net10.0/Mission06_Hu.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mission06_Hu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Hu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41dd52bd45c3394fd1f9ef65d48c325108dbd703")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6ccc44c31aa3750844ef572efc6c39ba2bcbbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Hu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Hu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
